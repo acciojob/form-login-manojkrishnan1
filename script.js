895bf1bc686ead19888f7382e351cf0827565da0
manojkrishnan1 <127621202+manojkrishnan1@users.noreply.github.com>
2023-04-19 19:16:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
-    //Write your code here
-
+		    let elements = document.querySelectorAll('#form1>input');
+            const firstName = elements[0];
+            const lastName = elements[1];
+            alert(firstName.value +' '+ lastName.value);
 }
